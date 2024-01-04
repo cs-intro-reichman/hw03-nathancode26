004bea717fc9e89e931ce32fc2525601e1ad7359
nathancode26 <154314837+nathancode26@users.noreply.github.com>
2024-01-04 17:51:21 +0200

    Update UniqueChars.java

--- UniqueChars.java
+++ UniqueChars.java
@@ -4,14 +4,25 @@ public class UniqueChars {
         String str = args[0];
         System.out.println(uniqueChars(str));
     }
-
     /**
      * Returns a string which is identical to the original string, 
      * except that all the duplicate characters are removed,
      * unless they are space characters.
      */
     public static String uniqueChars(String s) {
+        String ans = "" + s.charAt(0) ;
+        int i = 1;
+        while(i<s.length()){
+            char c = s.charAt(i);
+            if (i == (s.indexOf(c))){
+                ans = ans + c;
+            }
+            else{
+                ans = ans + "";
+            }
+            i++;
+        } 
         // Replace the following statement with your code
-        return null;
+        return ans;
     }
 }
